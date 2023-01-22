ee26fca000fbf5c55a172d7f798bf97658d7764f
Harpreet Singh <hsingh.161297@gmail.com>
2023-01-22 19:31:21 +0530

    Added db connectivity

--- dujell-server/src/app.controller.ts
+++ dujell-server/src/app.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { AppService } from './app.service';
-
-@Controller()
+import * as path from 'path';
+import * as sqlite from 'sqlite3';
+@Controller('/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello(); // Response from the server
+  }
+
+  @Post('login')
+  postLogin(@Req() req: Request, @Res() res: Response): any {
+    const db = new sqlite.Database(path.join(__dirname, 'db', 'users.db'));
+    const { username, password } = req.body;
+    console.log('from client: ', req.body);
+    
+
+    function callbackFunction(err: any, rows: any) {
+      if (err) return console.log('err occurred');
+      for (let i = 0; i < rows.length; i++) {
+        const row = rows[i];
+        console.log({dbusername: row.username, username});
+        
+        if (row.username === username && row.password === password) {
+          return res.status(200).send(JSON.stringify('{"message": "ok"}'));
+        }
+      }
+
+      return res.status(500).send(JSON.stringify('{"message": "error"}'));
+    }
+
+    db.all('SELECT * from users', callbackFunction);
   }
 }
--- dujell/src/App.tsx
+++ dujell/src/App.tsx
@@ -19,15 +19,38 @@ function App() {
     setPage(Page.LOGIN);
   }
 
+  function handleLogin(){
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://localhost:3000/login');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.onreadystatechange = function() {
+      if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){
+        console.log('login credentials are good');
+      }
+    }
+    xhr.send(JSON.stringify({ username, password }))
+
+    console.log({ username, password });
+    
+  }
+
   return (
-    <div>
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
+    <div className='dujell-font' style={{ height: '100%', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <div style={{ maxWidth: '20em', display: 'flex', flexDirection: 'column' }}>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          { page === Page.LOGIN && <h2>Login Here</h2> }
+          { page === Page.REGISTER && <h2>Register Here</h2> }
+        </div>
         { page === Page.REGISTER && <InputField placeholder='Email' type='email' /> }
-        { <InputField placeholder='Username' type='text' /> }
-        { <InputField placeholder='Password' type='password' /> }
+        { <InputField id='username' placeholder='Username' type='text' /> }
+        { <InputField id='password' placeholder='Password' type='password' /> }
         { page === Page.REGISTER && <InputField placeholder='Confirm Password' type='password' /> }
         { page === Page.REGISTER && <InputField placeholder='Mobile no' type='text' /> }
-        { page === Page.LOGIN && <InputButton>Login</InputButton> }
+        { page === Page.LOGIN && <InputButton onClick={handleLogin} >Login</InputButton> }
         { page === Page.LOGIN && <InputButton onClick={handleCreateAccount}>Create an account</InputButton> }
         { page === Page.REGISTER && <InputButton onClick={handleAlreadyAccount}>Already have an account</InputButton> }
         { page === Page.REGISTER && <InputButton>Register</InputButton> }
@@ -41,7 +64,7 @@ function InputField(props: any){
   // Custom INPUT Component/Element
 
   return(
-    <input style={{height: '3em'}} placeholder={props.placeholder} type={props.type} />
+    <input id={props.id} style={{ marginTop: '1.5em', height: '3em'}} placeholder={props.placeholder} type={props.type}/>
   );
 }
 
@@ -50,7 +73,7 @@ function InputButton(props: any){
   // Custom BUTTON Component/Element
 
   return(
-    <button style={{height: '3em'}} onClick={props.onClick} >{props.children}</button>
+    <button style={{ marginTop: '1.5em', height: '3em'}} onClick={props.onClick} >{props.children}</button>
   );
 }
 
