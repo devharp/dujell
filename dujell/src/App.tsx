d238f5a65fa445abca1fd88e24894f8670ac086b
Harpreet Singh <hsingh.161297@gmail.com>
2023-01-22 17:01:51 +0530

    Added Login/Register HTML UI Page

--- dujell/src/App.tsx
+++ dujell/src/App.tsx
@@ -2,33 +2,56 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+enum Page{
+  LOGIN = 0,
+  REGISTER = 1,
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [page, setPage] = useState(Page.LOGIN); // React Hook
+  
+  function handleCreateAccount(){
+    // Setting LOGIN Page to REGISTER Page
+    setPage(Page.REGISTER);
+  }
+  function handleAlreadyAccount(){
+    // Setting REGISTER Page to LOGIN Page
+    setPage(Page.LOGIN);
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        { page === Page.REGISTER && <InputField placeholder='Email' type='email' /> }
+        { <InputField placeholder='Username' type='text' /> }
+        { <InputField placeholder='Password' type='password' /> }
+        { page === Page.REGISTER && <InputField placeholder='Confirm Password' type='password' /> }
+        { page === Page.REGISTER && <InputField placeholder='Mobile no' type='text' /> }
+        { page === Page.LOGIN && <InputButton>Login</InputButton> }
+        { page === Page.LOGIN && <InputButton onClick={handleCreateAccount}>Create an account</InputButton> }
+        { page === Page.REGISTER && <InputButton onClick={handleAlreadyAccount}>Already have an account</InputButton> }
+        { page === Page.REGISTER && <InputButton>Register</InputButton> }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
+}
+
+function InputField(props: any){
+
+  // Custom INPUT Component/Element
+
+  return(
+    <input style={{height: '3em'}} placeholder={props.placeholder} type={props.type} />
+  );
+}
+
+function InputButton(props: any){
+
+  // Custom BUTTON Component/Element
+
+  return(
+    <button style={{height: '3em'}} onClick={props.onClick} >{props.children}</button>
+  );
 }
 
 export default App
